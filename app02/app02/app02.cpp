e0eeae64a3a927bbb8192da4b509b03dbd480a90
yuy <yy_dgs@126.com>
2021-04-20 22:34:44 +0800

    捕获空格和字符输入

---
+++ app02/app02/app02.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+#include <graphics.h>
+#include <conio.h>
+#include <stdio.h>
+
+int main()
+{
+    //std::cout << "Hello World!\n";
+
+    while (1) {
+        if (_kbhit())
+        {
+            char input = _getch();
+            std::cout << input << std::endl;
+            if (input == ' ')
+                std::cout << "按下了空格。。。\n" << std::endl;
+            
+        }
+    }
+
+    return 0;
+}
