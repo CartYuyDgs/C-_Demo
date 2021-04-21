c76c0a84c6c8a3f41932907847976586b98b1897
yuy <yy_dgs@126.com>
2021-04-21 23:43:50 +0800

    aa

--- app02/app02/app02.cpp
+++ app02/app02/app02.cpp
@@ -4,9 +4,58 @@
 #include <conio.h>
 #include <stdio.h>
 
+
+int main() {
+    float width, height, g;
+    float ball_x, ball_y, ball_vy, radius;
+    
+
+    width = 600;
+    height = 400;
+    g = 0.6;
+
+    initgraph(width, height);
+
+    radius = 10;
+    ball_x = width / 4;
+    ball_y = height - radius;
+    ball_vy = 0;
+
+    while (1)
+    {
+        
+        if (_kbhit())
+        {
+            char input = _getch();
+            if (input == ' ')
+            {
+                ball_vy = -16;
+            }
+        }
+
+        ball_vy = ball_vy + g;
+        ball_y = ball_y + ball_vy;
+
+        if (ball_y >= height - radius)
+        {
+            ball_vy = 0;
+            ball_y = height - radius;
+        }
+
+        clearcliprgn();
+        fillcircle(ball_x, ball_y, radius);
+        Sleep(10);
+    }
+    closegraph();
+    return 0;
+}
+
+/*
+
 int main()
 {
     //std::cout << "Hello World!\n";
+    /*
 
     while (1) {
         if (_kbhit())
@@ -18,6 +67,9 @@ int main()
             
         }
     }
+    
 
+    ball();
     return 0;
 }
+*/
